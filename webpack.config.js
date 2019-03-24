36f8c4369c61f2d49bebd7c7561ca8a8d56e269d
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-03-24 12:11:37 +0100

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
